d833a8cb728e9809d61117184cd0131fe582df7c
EduardoAfInacio <eduafinacio@gmail.com>
2023-07-24 14:40:31 -0300

    Alteração topologia.

--- mininet_topology.py
+++ mininet_topology.py
@@ -1,21 +1,22 @@
 from mininet.net import Mininet
 from mininet.node import RemoteController
+from mininet.node import Controller, OVSSwitch
 from mininet.cli import CLI
+from mininet.log import setLogLevel
 from mininet.link import TCLink
+import os
 
 def create_topology():
-    net = Mininet(controller=RemoteController, link=TCLink)
+    net = Mininet(controller=Controller, switch=OVSSwitch, link=TCLink)
+    net.addController('c0', controller=RemoteController, ip='172.28.121.175', port=6633)
 
-    # Adicionando o controlador Ryu
-    c0 = net.addController('c0', controller=RemoteController, ip='172.28.121.175', port=6633)
+    # Criação dos switches
+    s1 = net.addSwitch('s1', protocols='OpenFlow13')
+    s2 = net.addSwitch('s2', protocols='OpenFlow13')
+    s3 = net.addSwitch('s3', protocols='OpenFlow13')
+    s4 = net.addSwitch('s4', protocols='OpenFlow13')
 
-    # Criando switches
-    s1 = net.addSwitch('s1')
-    s2 = net.addSwitch('s2')
-    s3 = net.addSwitch('s3')
-    s4 = net.addSwitch('s4')
-
-    # Criando hosts
+    # Criação dos hosts
     h1 = net.addHost('h1', ip='10.0.0.1')
     h2 = net.addHost('h2', ip='10.0.0.2')
     h3 = net.addHost('h3', ip='10.0.0.3')
@@ -27,7 +28,7 @@ def create_topology():
     h9 = net.addHost('h9', ip='10.0.0.9')
     h10 = net.addHost('h10', ip='10.0.0.10')
 
-    # Criando links entre switches e hosts
+    # Criação das conexões
     net.addLink(s1, h1)
     net.addLink(s1, h2)
     net.addLink(s2, h3)
@@ -36,29 +37,18 @@ def create_topology():
     net.addLink(s3, h6)
     net.addLink(s4, h7)
     net.addLink(s4, h8)
-
-    # Criando links entre switches
     net.addLink(s1, s2)
     net.addLink(s1, s3)
     net.addLink(s2, s4)
     net.addLink(s3, s4)
-
-    # Iniciando a rede
+    net.addLink(s3, h9)
+    net.addLink(s4, h10)
+    
     net.start()
-
-    # Configurando o controlador para os switches
-    for switch in net.switches:
-        switch.start([c0])
-
-    # Adicionando uma rota padrão para cada host
-    for host in net.hosts:
-        host.cmd('route add default gw 10.0.0.1')
-
-    # Iniciando a interface de linha de comando do Mininet
     CLI(net)
-
-    # Parando a rede
     net.stop()
 
 if __name__ == '__main__':
+    setLogLevel('info')
+    os.system('mn -c')
     create_topology()
